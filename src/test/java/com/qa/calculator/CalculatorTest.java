744398384cf969a83122e58dc24f95f66ab668e9
JavaGoodDay <callummbailey98@gmail.com>
2019-06-24 16:20:57 +0100

    Commit

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -14,5 +14,23 @@ public class CalculatorTest {
 		int result = calculator.add(2,2);
 		Assert.assertEquals(4, result);
 	}
+	public void multiplyTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.multiply(2,2);
+		Assert.assertEquals(4, result);
+	}
+
+	public void divideTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(2,2);
+		Assert.assertEquals(4, result);
+	}
+
+	public void subtractTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(2,2);
+		Assert.assertEquals(4, result);
+	}
+
 
 }
